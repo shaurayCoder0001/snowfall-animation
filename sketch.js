27bcb2e82e86075dd502b968e94090b87e44a2d4
shaurayCoder0001 <83632832+shaurayCoder0001@users.noreply.github.com>
2021-05-28 13:32:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
-
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint  = Matter. Constraint;
 
 var engine, world;
 var backgroundImg;
@@ -7,53 +10,51 @@ var bg  ;
 var Snowfall1 , Snowfall2
 
 function preload() {
-  Snowfall1Img = loadImage("snow4.webp")
-  Snowfall2Img = loadImage("snow5.webp")
   getBackgroundImg();
-
+ 
 }
 
 function setup() {
   createCanvas(800,400);
- 
-  Snowfall1=createSprite(400, 200, 50,50);
-	Snowfall1.addImage(Snowfall1Img)
-	Snowfall1.scale=0.2
 
-	Snowfall2=createSprite(400, 70, 50,50);
-Snowfall2.addImage(Snowfall2Img)
-	Snowfall2.scale=0.2
+  engine = Engine.create();
+   world = engine.world;
+ 
+ 
+   Snowfall1 = new Snow ( 400,200,50,50)
+   Snowfall2 = new Snow ( 100,200,50,50)
 }
 
 function draw() {
 
-  if(backgroundImg){
-    background(backgroundImg);
-   }
+  if(backgroundImg)
+  background(backgroundImg); 
+  Engine.update(engine);
 
+  Snowfall1.display()
+  Snowfall2.display()
 
-   ;
-
-   drawSprites();
-
-  
+ 
 }
 
 
+
+ 
 async function getBackgroundImg(){
    
   var response = await fetch ("https://worldtimeapi.org/api/timezone/Asia/Tokyo")
   
   var responseJSON = await response.json()
   
+  
   var datetime = responseJSON.datetime;
   var hour = datetime.slice (11,13)
 
  
-  if(hour >=04 && hour<=14 ){
+  if(hour >=04 && hour<=16){
       bg = "snow1.jpg"
  }
- else if(hour >=14 && hour<=20 ){
+ else if(hour >=16 && hour<=21 ){
    bg = "snow2.jpg"
 }
 
@@ -67,4 +68,17 @@ else{
    
       }
 
+
+      function mouseDragged(){
+       
+            
+        Matter.Body.setPosition(Snowfall1.body, {x: mouseX , y: mouseY});
+          
+        Matter.Body.setPosition(Snowfall2.body, {x: mouseX , y: mouseY});
+       
+      
+          }
     
+    
+    
+   
